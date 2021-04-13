465b4833205778ce227674bcc4f0058bc7c466c6
3202448109@qq.com <3202448109@qq.com>
2021-04-13 21:01:54 +0800

    admin.py .gitignore __init__.py apps.py models.py tests.py views.py

---
+++ edu_api/apps/user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+class UserInfo(AbstractUser):
+    """用户模型"""
+    phone = models.CharField(max_length=11, unique=True, verbose_name="手机号")
+    image = models.ImageField(upload_to='user', blank=True, null=True, verbose_name="用户头像")
+
+    class Meta:
+        db_table = "bz_user"
+        verbose_name = "用户表"
+        verbose_name_plural = verbose_name
+
+# 将自定义的用户模型注册为django默认的user表
+AUTH_USER_MODEL = 'user.UserInfo'
